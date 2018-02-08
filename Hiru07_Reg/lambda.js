e15e5d896f893084d0c158af8bd622d5f57004ea
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-08 18:16:41 +0530

    describe stream

--- Hiru07_Reg/lambda.js
+++ Hiru07_Reg/lambda.js
@@ -3,6 +3,16 @@ const kinesis = new AWS.Kinesis();
 exports.handler = function (event, context, callback) {
 
 
+	kinesis.describeStream({
+		StreamName: 'Hiru_test'
+	}).promise()
+		.then(data => {
+			// your logic goes here
+		})
+		.catch(err => {
+			// error handling goes here
+		});
+
 
 
 	//callback(null, 'Successfully executed');
