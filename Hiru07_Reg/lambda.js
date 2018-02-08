45b84a0392eb55ca321b1c1b95218b7e9cb12891
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-08 18:27:32 +0530

    describe stream

--- Hiru07_Reg/lambda.js
+++ Hiru07_Reg/lambda.js
@@ -2,9 +2,9 @@ let AWS = require('aws-sdk');
 const kinesis = new AWS.Kinesis();
 exports.handler = function (event, context, callback) {
 
-
 	kinesis.describeStream({
-		StreamName: 'Hiru_test'
+		StreamName: 'Hiru_test',
+		ExclusiveStartShardId: 'shardId-000000000000'
 	}).promise()
 		.then(data => {
 			// your logic goes here
