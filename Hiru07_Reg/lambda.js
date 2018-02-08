ec981bbf924fd057963dc28d1c444971a353467d
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-08 16:46:47 +0530

    kinesis comment

--- Hiru07_Reg/lambda.js
+++ Hiru07_Reg/lambda.js
@@ -1,6 +1,23 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const kinesis = new AWS.Kinesis();
+exports.handler = function (event, context, callback) {
+
+	kinesis.putRecord({
+		Data: 'Testedit',
+		PartitionKey: '001',
+		StreamName: 'Hiru_test_1',
+		ExplicitHashKey: '012',
+		SequenceNumberForOrdering: '123'
+	}).promise()
+		.then(data => {
+			// your logic goes here
+			console.log('test : ', data);
+		})
+		.catch(err => {
+			// error handling goes here
+			console.log('sample : ', err);
+		});
 
 
-	callback(null,'Successfully executed');
+	//callback(null, 'Successfully executed');
 }
