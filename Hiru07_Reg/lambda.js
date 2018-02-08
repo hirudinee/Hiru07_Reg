e95dcc2b24a88512abd6cc6745adbe9736635523
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-08 17:28:42 +0530

    updated no of shards 2

--- Hiru07_Reg/lambda.js
+++ Hiru07_Reg/lambda.js
@@ -2,6 +2,7 @@ let AWS = require('aws-sdk');
 const kinesis = new AWS.Kinesis();
 exports.handler = function (event, context, callback) {
 
+//added comments
 	kinesis.putRecord({
 		Data: 'Testedit',
 		PartitionKey: '001',
