1b34c75306409007a01f67f468d22b8189c7ca5e
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-08 18:14:20 +0530

    removed kinesis

--- Hiru07_Reg/lambda.js
+++ Hiru07_Reg/lambda.js
@@ -2,22 +2,7 @@ let AWS = require('aws-sdk');
 const kinesis = new AWS.Kinesis();
 exports.handler = function (event, context, callback) {
 
-	//added comments
-	kinesis.putRecord({
-		Data: 'Testedit2',
-		PartitionKey: 'shardId-000000000001',
-		StreamName: 'Hiru_test_1'
-	}).promise()
-		.then(data => {
-			// your logic goes here
-			console.log('test : ', data);
-			callback(null, data);
-		})
-		.catch(err => {
-			// error handling goes here
-			console.log('sample : ', err);
-			callback(null, err);
-		});
+
 
 
 	//callback(null, 'Successfully executed');
